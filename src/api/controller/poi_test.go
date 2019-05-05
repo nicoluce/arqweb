cb860595eef3c283ed8aafa227ac635fa1629341
Fernando Balboa <fernando.balboa@mercadolibre.com>
2019-05-05 04:12:02 -0300

    Add controller POI search test

--- src/api/controller/poi_test.go
+++ src/api/controller/poi_test.go
@@ -4,12 +4,15 @@ import (
 	"encoding/json"
 	"github.com/fernetbalboa/arqweb/src/api/config"
 	"github.com/fernetbalboa/arqweb/src/api/controller"
+	"github.com/fernetbalboa/arqweb/src/api/domain"
 	"github.com/fernetbalboa/arqweb/src/api/mock"
 	"github.com/fernetbalboa/arqweb/src/api/test"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
+	"strconv"
 	"strings"
 	"testing"
 )
@@ -20,24 +23,30 @@ func init() {
 
 func TestAddPOI(t *testing.T) {
 	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	addPOIEndpoint := "/poi"
 
 	t.Run("Request is not GeoJson", func(t *testing.T) {
 		//Given
 		POIController := &controller.POIController{}
-		badRequestBody := "{\"invalidField\": \"not a GeoJson\"}"
+		badRequestBody := `{"invalidField": "not a GeoJson"}`
 
 		r := config.ConfiguredRouter()
-		r.POST("/poi", POIController.AddPOI)
+		r.POST(addPOIEndpoint, POIController.AddPOI)
 		w := httptest.NewRecorder()
-		req, _ := http.NewRequest("POST", "/poi", strings.NewReader(badRequestBody))
+		req, _ := http.NewRequest("POST", addPOIEndpoint, strings.NewReader(badRequestBody))
 
 		//When
 		r.ServeHTTP(w, req)
 
 		//Then
 		assert.Equal(t, http.StatusBadRequest, w.Code)
-		assert.Equal(t, "{\"message\":\"There was a problem with the request\"," +
-			"\"error\":\"bad_request\",\"status\":400,\"cause\":[\"Error parsing POI. It should be a GeoJson feature\"]}",
+		assert.Equal(t, `{
+  "message":"There was a problem with the request",
+  "error":"bad_request",
+  "status":400,
+  "cause":["Error parsing POI. It should be a GeoJson feature"],
+}`,
 			w.Body.String())
 
 	})
@@ -48,9 +57,9 @@ func TestAddPOI(t *testing.T) {
 		POIController := &controller.POIController{POIStorage:storageMock}
 
 		r := config.ConfiguredRouter()
-		r.POST("/poi", POIController.AddPOI)
+		r.POST(addPOIEndpoint, POIController.AddPOI)
 		w := httptest.NewRecorder()
-		req, _ := http.NewRequest("POST", "/poi", strings.NewReader(test.DefaultGeoJsonFeatureString()))
+		req, _ := http.NewRequest("POST", addPOIEndpoint, strings.NewReader(test.DefaultGeoJsonFeatureString()))
 
 		savedPOI := test.DefaultPOI()
 		storageMock.EXPECT().SaveFeature(test.DefaultGeoJsonFeature()).Return(savedPOI, nil)
@@ -61,6 +70,67 @@ func TestAddPOI(t *testing.T) {
 		//Then
 		assert.Equal(t, http.StatusCreated, w.Code)
 		respBody, _ := json.Marshal(savedPOI)
+
+		assert.Equal(t, respBody,w.Body.Bytes())
+
+	})
+}
+
+func TestSearchPOI(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	searchPOIEndpoint := "/poi/search"
+
+	t.Run("Search POI successfully", func(t *testing.T) {
+		//Given
+		storageMock := mock.NewMockPOIStorage(ctrl)
+		POIController := &controller.POIController{POIStorage:storageMock}
+
+		category := "food"
+		maxLat := 10.63462
+		minLat := 5.45345
+		maxLong := 30.3523523
+		minLong := 24.62311424
+
+		queryParams := url.Values{}
+		queryParams.Add("category", category)
+		queryParams.Add("maxLat", strconv.FormatFloat(maxLat, 'f', 10, 64))
+		queryParams.Add("minLat", strconv.FormatFloat(minLat, 'f', 10, 64))
+		queryParams.Add("maxLong", strconv.FormatFloat(maxLong, 'f', 10, 64))
+		queryParams.Add("minLong", strconv.FormatFloat(minLong, 'f', 10, 64))
+		queryParams.Add("bound", strconv.FormatBool(true))
+
+
+		r := config.ConfiguredRouter()
+		r.GET(searchPOIEndpoint, POIController.SearchPOI)
+		w := httptest.NewRecorder()
+		req, _ := http.NewRequest("GET", searchPOIEndpoint, nil)
+		req.URL.RawQuery = queryParams.Encode()
+
+		savedPOI1 := test.DefaultPOI()
+		savedPOI2 := test.DefaultPOI()
+		savedPOI2.Id = test.NewDocumentId()
+
+		foundPOIs := []*domain.PointOfInterest{savedPOI1, savedPOI2}
+
+		filters := &domain.POIFilter{
+			Category:category,
+			MaxLong:maxLong,
+			MaxLat:maxLat,
+			MinLat:minLat,
+			MinLong:minLong,
+			Bound:true,
+			Limit:controller.DefaultSearchLimit,
+		}
+
+		storageMock.EXPECT().Search(filters).Return(foundPOIs, nil)
+
+		//When
+		r.ServeHTTP(w, req)
+
+		//Then
+		assert.Equal(t, http.StatusOK, w.Code)
+		respBody, _ := json.Marshal(foundPOIs)
 
 		assert.Equal(t, respBody,w.Body.Bytes())
 
