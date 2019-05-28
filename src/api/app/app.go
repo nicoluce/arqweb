7d04293c2326cf12c9c86031b89cb07d14467990
nluce <nico182.luce@gmail.com>
2019-05-28 19:52:46 -0300

    Fixing port

--- src/api/app/app.go
+++ src/api/app/app.go
@@ -5,10 +5,12 @@
 package app
 
 import (
+	"fmt"
 	"github.com/nicoluce/arqweb/src/api/config"
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
 	"os"
+	"strings"
 )
 
 // Router is the base Gin Router used for mapping endpoints.
@@ -18,6 +20,12 @@ var Router *gin.Engine
 func Start() {
 	Router = config.ConfiguredRouter()
 	LoadEndpoints()
+
+	fmt.Println()
+	for _, e := range os.Environ() {
+		pair := strings.Split(e, "=")
+		fmt.Println(pair[0])
+	}
 
 	port := os.Getenv("$PORT")
 	log.Infof("Port: %s", port)
