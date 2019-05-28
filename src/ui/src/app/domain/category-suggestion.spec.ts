986c6707434dfcbfb7c7540aa995d65144973f75
nluce <nico182.luce@gmail.com>
2019-05-27 21:17:22 -0300

    dejo solo el server para ver algo de heroku

--- src/ui/src/app/domain/category-suggestion.spec.ts
+++
@@ -1,7 +0,0 @@
-import { CategorySuggestion } from './category-suggestion';
-
-describe('CategorySuggestion', () => {
-  it('should create an instance', () => {
-    expect(new CategorySuggestion()).toBeTruthy();
-  });
-});
--- src/ui/src/app/domain/point-of-interest.spec.ts
+++
@@ -1,7 +0,0 @@
-import { PointOfInterest } from './point-of-interest';
-
-describe('PointOfInterest', () => {
-  it('should create an instance', () => {
-    expect(new PointOfInterest()).toBeTruthy();
-  });
-});
