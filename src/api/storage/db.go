a4a04d6c36bbe55987aeda41568647da77e756c1
nluce <nico182.luce@gmail.com>
2019-05-28 19:07:22 -0300

    new db url

--- src/api/storage/db.go
+++ src/api/storage/db.go
@@ -38,7 +38,7 @@ type ICollection interface {
 
 func getMongoDBClient() (*mongo.Client, error) {
 	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://<arqweb>:<arqweb123>@ds263146.mlab.com:63146/arqweb"))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://arqweb:arqweb123@ds263146.mlab.com:63146/arqweb"))
 
 	if err != nil {
 		return nil, apierror.Wrap(err, "Could not connect to MongoDB")
